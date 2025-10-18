79d6f87a0bb17b2ae178368d2e2a903091dfa5e8
sachin3337 <sachinsubhashghusinge@gmail.com>
2025-10-18 23:04:38 +0530

    correct module

--- user/models.py
+++ user/models.py
@@ -20,4 +20,12 @@ class FoodItemPurchase(models.Model):
         (9, "September"), (10, "October"), (11, "November"), (12, "December")
     ]
 
-    
+    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE, related_name='purchases')
+    quantity_bought = models.PositiveIntegerField()
+    month_bought = models.IntegerField(choices=MONTH_CHOICES)  # Month choices as names
+    year_bought = models.IntegerField(default=timezone.now().year)  # Stores the year of purchase, default is current year
+    amount_wasted = models.PositiveIntegerField(default=0, null=True, blank=True)  # Amount wasted, filled at the end of the month
+
+    def __str__(self):
+        month_name = dict(self.MONTH_CHOICES).get(self.month_bought, "Unknown Month")
+        return f"{self.quantity_bought} of {self.food_item.name} bought in {month_name} {self.year_bought}"
